6a55259eb1d36c4f40ba117c300852cf0644e22c
whiz25 <afanimartin@gmail.com>
2019-03-10 17:25:12 +0300

    Add functionality for checking acronyms and words that start with capital letters

--- dictionary.py
+++ dictionary.py
@@ -7,12 +7,17 @@ data =  json.load(open("dictionary.json"))
 def retrieve_definition(word):
     if word in data:
         return data[word]
+    elif word.title() in data: 
+        return data[word.title()] # return definitions of words that start with capital
+    elif word.upper() in data:
+        return data[word.upper()] # return definitions of words that are capital
+
     return "That word does not exist. Please double check!"
 
 # take input from user,
 # retrieve the definition
 
-user_input = input("Enter a word: ")
+user_input = input("Enter a word: ").lower()
 
 print(retrieve_definition(user_input))
 
